6ebe0253cb9ea40c4448f7b4c5cd46b52488deaf
piersy <pierspowlesland@gmail.com>
2015-04-08 12:29:55 +0100

    Initial commit

---
+++ src/test/java/BasicTesseractExampleTest.java
@@ -0,0 +1,32 @@
+import org.bytedeco.javacpp.*;
+import org.junit.Test;
+
+import static org.bytedeco.javacpp.lept.*;
+import static org.bytedeco.javacpp.tesseract.*;
+
+public class BasicTesseractExampleTest {
+    
+    @Test
+    public void givenTessBaseApi_whenImageOcrd_thenTextDisplayed() throws Exception {
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+        // Initialize tesseract-ocr with English, without specifying tessdata path
+        if (api.Init(".", "ENG") != 0) {
+            System.err.println("Could not initialize tesseract.");
+            System.exit(1);
+        }
+
+        // Open input image with leptonica library
+        PIX image = pixRead("test.png");
+        api.SetImage(image);
+        // Get OCR result
+        outText = api.GetUTF8Text();
+        System.out.println("OCR output:\n" + outText.getString());
+
+        // Destroy used object and release memory
+        api.End();
+        outText.deallocate();
+        pixDestroy(image);
+    }
+}
